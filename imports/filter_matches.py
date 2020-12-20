7169f217cf2db0b084d8c792a160f27420eed893
Hasan-Alsulaiman <careerforma@gmail.com>
2020-12-20 13:58:16 +0300

    first

---
+++ imports/filter_matches.py
@@ -0,0 +1,12 @@
+import numpy as np
+def filter_matches(kp1, kp2, matches, ratio = 0.75):
+    mkp1, mkp2 = [], []
+    for m in matches:
+        if len(m) == 2 and m[0].distance < m[1].distance * ratio:
+            m = m[0]
+            mkp1.append( kp1[m.queryIdx] )
+            mkp2.append( kp2[m.trainIdx] )
+    p1 = np.float32([kp.pt for kp in mkp1])
+    p2 = np.float32([kp.pt for kp in mkp2])
+    kp_pairs = zip(mkp1, mkp2)
+    return p1, p2, list(kp_pairs)
---
+++ main.py
@@ -0,0 +1,38 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
+from imports.filter_matches import filter_matches
+from imports.explore_match import explore_match
+
+
+# path of image 1
+path1 = "./images/img1.jpg"
+
+# path of image 2
+path2 = "./images/img2.jpg"
+
+img1 = cv.imread(path1,cv.IMREAD_GRAYSCALE) 
+img2 = cv.imread(path2,cv.IMREAD_GRAYSCALE) 
+
+
+
+detector = cv.BRISK_create()
+norm = cv.NORM_HAMMING
+kp1, desc1 = detector.detectAndCompute(img1, None)
+kp2, desc2 = detector.detectAndCompute(img2, None)
+FLANN_INDEX_LSH    = 6
+flann_params= dict(algorithm = FLANN_INDEX_LSH,
+                               table_number = 6, # 12
+                               key_size = 12,     # 20
+                               multi_probe_level = 1) #2
+matcher = cv.FlannBasedMatcher(flann_params, {})
+raw_matches = matcher.knnMatch(desc1, trainDescriptors = desc2, k = 2) #2
+p1, p2, kp_pairs = filter_matches(kp1, kp2, raw_matches)
+if len(p1) >= 4:
+    H, status = cv.findHomography(p1, p2, cv.RANSAC, 5.0)
+    print('%d / %d  inliers/matched' % (np.sum(status), len(status)))
+else:
+    H, status = None, None
+    print('%d matches found, not enough for homography estimation' % len(p1))
+vis = explore_match("win", img1, img2, kp_pairs, status, H)
+cv.imwrite("match.jpg", vis)
