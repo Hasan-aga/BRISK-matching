a6897be843927c16c20f79a588575c6d018d4990
Hasan-Alsulaiman <careerforma@gmail.com>
2020-12-20 20:18:20 +0300

    final

--- imports/filter_matches.py
+++ imports/filter_matches.py
@@ -1,5 +1,6 @@
 import numpy as np
 def filter_matches(kp1, kp2, matches, ratio = 0.75):
+    # 
     mkp1, mkp2, good= [], [], []
     for m in matches:
         if len(m) == 2 and m[0].distance < m[1].distance * ratio:
